4cdda7e312c65cba94820e5ea70a43a0d447509e
Koichiro Tamura <koichiriot11@gmail.com>
2018-03-27 16:14:27 +0900

    debug

--- create_tf_record.py
+++ create_tf_record.py
@@ -2,10 +2,17 @@
 """
 create tf record
 """
-
+import argparse
 from data_loader import DataLoaderCIFAR10 as DataLoader
 
 if __name__ == '__main__':
 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-n', '--num_shard', type=int, default=1, help='numer of output file')
+    parser.add_argument('-l', '--limit', type=int, default=None, help='limit the dataset size. for debug use')
+
+    args = parser.parse_args()
+
     data_loader = DataLoader()
-    data_loader.main()
+    data_loader.main(num_shard=args.num_shard, limit=args.limit)
